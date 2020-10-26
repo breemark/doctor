a0c8716d7c13ab8752c4f574f97fa1308e8bd8c6
breemark <britomaximiliano83@gmail.com>
2020-10-26 14:52:55 +0700

    readme

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     @if(Session::has('message'))
-        <div class="alert alert-success">{{Session::get('message')}}</div>
-        @endif
+    <div class="alert alert-success">{{Session::get('message')}}</div>
+    @endif
     <div class="row ">
-        
+
         <div class="col-md-3">
             <div class="card">
                 <div class="card-header">User Profile</div>
@@ -32,21 +32,21 @@
                             <label>Name</label>
                             <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{auth()->user()->name}}">
                             @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+
                         </div>
                         <div class="form-group">
                             <label>Address</label>
                             <input type="text" name="address" class="form-control" value="{{auth()->user()->address}}">
-                            
+
                         </div>
                         <div class="form-group">
                             <label>Phone number</label>
                             <input type="text" name="phone_number" class="form-control" value="{{auth()->user()->phone_number}}">
-                            
+
                         </div>
                         <div class="form-group">
                             <label>Gender</label>
@@ -56,26 +56,26 @@
                                 <option value="female" @if(auth()->user()->gender==='female')selected @endif>Female</option>
                             </select>
                             @error('gender')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                       
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+
                             <div class="form-group">
-                            <label>Bio</label>
-                            <textarea name="description" class="form-control">{{auth()->user()->description}}</textarea>
-                            
+                                <label>Bio</label>
+                                <textarea name="description" class="form-control">{{auth()->user()->description}}</textarea>
+
+                            </div>
+                            <div class="form-group">
+
+                                <button class="btn btn-primary" type="submit">Update</button>
+
+                            </div>
+
                         </div>
-                        <div class="form-group">
-                            
-                            <button class="btn btn-primary" type="submit">Update</button>
-                            
-                        </div>
-                            
-                        </div>
-                        
+
                     </form>
-                    
+
                 </div>
             </div>
         </div>
@@ -83,24 +83,24 @@
             <div class="card">
                 <div class="card-header">Update Image</div>
                 <form action="{{route('profile.pic')}}" method="post" enctype="multipart/form-data">@csrf
-                <div class="card-body">
-                    @if(!auth()->user()->image)
-                    <img src="/images/3Dz1og01c2vXjbjmfTskpLqdVGEB2Qmpg1DLROiR.png" width="120">
-                    @else 
-                     <img src="/profile/{{auth()->user()->image}}" width="120">
-                    @endif
-                    <br>
-                    <input type="file" name="file" class="form-control" required="">
-                    <br>
-                     @error('file')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    
-                </div>
-            </form>
+                    <div class="card-body">
+                        @if(!auth()->user()->image)
+                        <img src="/images/3Dz1og01c2vXjbjmfTskpLqdVGEB2Qmpg1DLROiR.png" width="120">
+                        @else
+                        <img src="/profile/{{auth()->user()->image}}" width="120">
+                        @endif
+                        <br>
+                        <input type="file" name="file" class="form-control" required="">
+                        <br>
+                        @error('file')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                        <button type="submit" class="btn btn-primary">Update</button>
+
+                    </div>
+                </form>
             </div>
         </div>
 
